e0d9b62bd018912b920a92c2e4becf3149adb872
Bahman World <bahman.world@gmail.com>
2025-12-22 10:53:07 +0330

    some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import Providers from "@/components/contexts/Providers";
 import "./globals.css";
 import "../public/fonts/remixicon/remixicon.css";
 import AppWrapper from "@/components/views/AppWrapper";
-import AppBottomTabs from "@/components/views/AppBottomTabs";
-import Icon from "@/components/fields/Icon";
 
 export const metadata: Metadata = {
   title: "Pewist",
